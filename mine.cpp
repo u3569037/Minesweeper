c4aae558d85d24449ab1e884752b83924d88a4b7
3035690775 <63488544+3035690775@users.noreply.github.com>
2020-05-09 17:01:33 +0800

    Add files via upload

--- mine.cpp
+++ mine.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 int endgame = 0;
@@ -9,11 +11,59 @@ const int SIZE = 9;     //size of map
 const int mines = 10;   //10 mines
 int board[SIZE][SIZE];
 char display_board[SIZE][SIZE];
+int flagc = 0;
+int stime = time(NULL);
+
+
+void hscore(){
+  string line;
+  int i, n = 0;
+  ifstream fin;
+  cout << "====================Highscore Board====================" << endl;
+  fin.open("mkdata.txt");
+  if (fin.fail()){
+    cout << "Error in file opening!" << endl;
+      exit(1);
+  }
+  while(getline(fin,line)){
+    n += 1;
+  }
+  fin.close();
+  fin.open("mkdata.txt");
+  if (fin.fail()){
+    cout << "Error in file opening!" << endl;
+      exit(1);
+  }
+  string * name = new string[n];
+  int * score = new int[n];
+  while(getline(fin,line)){
+    istringstream ins(line);
+    ins >> name[i];
+    ins >> score[i];
+    i++;
+	}
+  for (int i = 0; i < n - 1; i++){
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+			if (score[j] < score[min])
+				min = j;
+		swap(score[i], score[min]);
+    swap(name[i], name[min]);
+	}
+  for (int i = 0; i < n; i++)
+    cout << i+1 << "   " << setw(25) << name[i] << "           " << score[i] << "s" << endl;
+  fin.close();
+  delete [] name;
+  delete [] score;
+  cout << "=======================================================" << endl;
+}
+
 
 void genboard(){                   //initiation of the board
   int minepos = 0, minecnt = 0;
   cout << "Welcome to Minesweeper game ^_^" << endl;
   cout << "9x9 board." << endl;
+  hscore();
   for (int i = 0; i < SIZE; i++)
     for (int j = 0; j < SIZE; j++)
       board[i][j] = -1;         // value of -1 = unclicked and value of 0 = clicked
@@ -54,34 +104,37 @@ int num_of_mines(int pos){  //determine the number of mines surrouding the block
   int mines=0;
   if (pos/9+1<9 && pos/9+1>=0 && pos%9+1<9 && pos%9+1>=0){
     if (board[pos/9+1][pos%9+1]==1) mines++;
-  } 
+  }
   if (pos/9+1<9 && pos/9+1>=0 && pos%9<9 && pos%9>=0){
     if (board[pos/9+1][pos%9]==1) mines++;
   }
   if (pos/9+1<9 && pos/9+1>=0 && pos%9-1<9 && pos%9-1>=0){
     if (board[pos/9+1][pos%9-1]==1) mines++;
-  } 
+  }
   if (pos/9<9 && pos/9>=0 && pos%9+1<9 && pos%9+1>=0){
     if (board[pos/9][pos%9+1]==1) mines++;
-  } 
+  }
   if (pos/9<9 && pos/9>=0 && pos%9-1<9 && pos%9-1>=0){
     if (board[pos/9][pos%9-1]==1) mines++;
-  } 
+  }
   if (pos/9-1<9 && pos/9-1>=0 && pos%9+1<9 && pos%9+1>=0){
     if (board[pos/9-1][pos%9+1]==1) mines++;
-  } 
+  }
   if (pos/9-1<9 && pos/9-1>=0 && pos%9<9 && pos%9+1>=0){
     if (board[pos/9-1][pos%9]==1) mines++;
-  } 
+  }
   if (pos/9-1<9 && pos/9-1>=0 && pos%9+1<9 && pos%9+1>=0){
     if (board[pos/9-1][pos%9-1]==1) mines++;
-  } 
+  }
   return mines;
 }
 
 void mvboard(){
   char act = ' ';  // store the user action
-  int block[2];  // store the selected block
+  int block[2] = {-1, -1};  // store the selected block
+  char chk;
+  int temp = 0;
+  bool pass = 0;
   cout << "Choose an action: " << endl;
   cout << "F for flag/unflag a block with mine" << endl;
   cout << "C for clicking an unclicked block" << endl;
@@ -95,18 +148,48 @@ void mvboard(){
   }
   cout << "Please input the coordinates of the block" << endl;
   cout << "x-coordinate (horizontal axis): ";
-  cin >> block[0];
-  cout << "y-coordinate (vertical axis): ";
-  cin >> block[1];
-  while (board[block[1]][block[0]]==0){   //if clicked or flagged a clicked block
-      cout << "Error: The selected block is already clicked" << endl;
-      cout << "Please input again >w< " << endl;
-      cout << "Please input the coordinates of the block" << endl;
-      cout << "x-coordinate (horizontal axis): ";
-      cin >> block[0];
-      cout << "y-coordinate (vertical axis): ";
-      cin >> block[1];
+  cin >> chk;
+  if(isdigit(chk))
+    if((chk >= '0') && (temp <='8'))
+      pass = 1;
+  while (pass == 0){
+  cout << "Error: Invalid input" << endl;
+  cout << "Please input the coordinates of the block" << endl;
+  cout << "x-coordinate (horizontal axis): ";
+  cin >> chk;
+  if(isdigit(chk))
+    if((chk >= '0') && (temp <='8'))
+      pass = 1;
+  cin.clear();
+  fflush(stdin);
   }
+  pass = 0;
+  cout << "Selected x: " << int(chk)-'0' << endl;
+  block[0] = int(chk)-'0';
+  cin.clear();
+  fflush(stdin);
+  cout << "Please input the coordinates of the block" << endl;
+  cout << "y-coordinate (horizontal axis): ";
+  cin >> chk;
+  if(isdigit(chk))
+    if((chk >= '0') && (temp <='8'))
+      pass = 1;
+  while (pass == 0){
+  cout << "Error: Invalid input" << endl;
+  cout << "Please input the coordinates of the block" << endl;
+  cout << "y-coordinate (horizontal axis): ";
+  cin >> chk;
+  if(isdigit(chk))
+    if((chk >= '0') && (temp <='8'))
+      pass = 1;
+  cin.clear();
+  fflush(stdin);
+  }
+  pass = 0;
+  block[1] = int(chk)-'0';
+  cout << "Selected y: " << int(chk)-'0' << endl;
+  cin.clear();
+  fflush(stdin);
   int *sempty= new int [81];   //declare a dynamic array to store the surrouding empty blocks
   for(int i = 0; i < 81; ++i){
     sempty[i] = -1;
@@ -117,16 +200,21 @@ void mvboard(){
     if (board[pos/9][pos%9]==2){ //unflag
       board[pos/9][pos%9]=-1;
       display_board[pos/9][pos%9] = 'X';
+      flagc--;
+      cout << "Remaining flags = " << 10 - flagc << endl;
     }
     else{  //flag
+      board[pos/9][pos%9]=2;
+      flagc++;
+      cout << "Remaining flags = " << 10 - flagc << endl;
       display_board[pos/9][pos%9] = 'F';
     }
   }
   if (act == 'C'){
     if (board[pos/9][pos%9]==1){ // if the user click on a block with mine
-       cout << "Bomb!!! You lose >w<" << endl; 
+       cout << "Bomb!!! You lose >w<" << endl;
        cout << "Add oil next time ^_^" << endl;
-       endgame=1; 
+       endgame=1;
     }
     else{
       display_board[pos/9][pos%9]='0'+num_of_mines(pos);
@@ -152,7 +240,7 @@ void mvboard(){
             display_board[pos/9+1][pos%9+1]='0'+num_of_mines(pos+9+1);
             board[pos/9+1][pos%9+1] = 0;
           }
-        } 
+        }
         if (pos/9+1<9 && pos/9+1>=0 && pos%9<9 && pos%9>=0){
           if (num_of_mines(pos+9)==0 && board[pos/9+1][pos%9]!=1){
             display_board[pos/9+1][pos%9] = ' ';
@@ -186,7 +274,7 @@ void mvboard(){
             display_board[pos/9+1][pos%9-1]='0'+num_of_mines(pos+9-1);
             board[pos/9+1][pos%9-1] = 0;
           }
-        } 
+        }
         if (pos/9<9 && pos/9>=0 && pos%9+1<9 && pos%9+1>=0){
           if (num_of_mines(pos+1)==0 && board[pos/9][pos%9+1]!=1){
             display_board[pos/9][pos%9+1] = ' ';
@@ -203,7 +291,7 @@ void mvboard(){
             display_board[pos/9][pos%9+1]='0'+num_of_mines(pos+1);
             board[pos/9][pos%9+1] = 0;
           }
-        } 
+        }
         if (pos/9<9 && pos/9>=0 && pos%9-1<9 && pos%9-1>=0){
           if (num_of_mines(pos-1)==0 && board[pos/9][pos%9-1]!=1){
             display_board[pos/9][pos%9-1] = ' ';
@@ -220,7 +308,7 @@ void mvboard(){
             display_board[pos/9][pos%9-1]='0'+num_of_mines(pos-1);
             board[pos/9][pos%9-1] = 0;
           }
-        } 
+        }
         if (pos/9-1<9 && pos/9-1>=0 && pos%9+1<9 && pos%9+1>=0){
           if (num_of_mines(pos-9+1)==0 && board[pos/9-1][pos%9+1]!=1){
             display_board[pos/9-1][pos%9+1] = ' ';
@@ -237,7 +325,7 @@ void mvboard(){
             display_board[pos/9-1][pos%9+1]='0'+num_of_mines(pos-9+1);
             board[pos/9-1][pos%9+1] = 0;
           }
-        } 
+        }
         if (pos/9-1<9 && pos/9-1>=0 && pos%9<9 && pos%9>=0){
           if (num_of_mines(pos-9)==0 && board[pos/9-1][pos%9]!=1){
             display_board[pos/9-1][pos%9] = ' ';
@@ -254,7 +342,7 @@ void mvboard(){
             display_board[pos/9-1][pos%9]='0'+num_of_mines(pos-9);
             board[pos/9-1][pos%9] = 0;
           }
-        } 
+        }
         if (pos/9-1<9 && pos/9-1>=0 && pos%9-1<9 && pos%9-1>=0){
           if (num_of_mines(pos-9-1)==0 && board[pos/9-1][pos%9-1]!=1){
             display_board[pos/9-1][pos%9-1] = ' ';
@@ -280,14 +368,47 @@ void mvboard(){
       }
     }
   }
-  delete [] sempty; 
+  delete [] sempty;
 }
+
+void storemk(){
+  string name;
+  int rec;
+  ofstream fout;
+  rec = difftime(time(NULL),stime);
+  cout << "Congradulations! You win the game in " << rec << " seconds!" << endl;
+  cout << "Please type your name for recording: ";
+  cin >> name;
+	fout.open("mkdata.txt", ios::app);
+	if (fout.fail()) {
+		cout << "Error in file opening!" << endl;
+      exit(1);
+  }
+  fout << name << " " << rec << endl;
+   fout.close();
+}
+
+void endchk(){
+  bool found = 0;
+  for (int i = 0; i < 9; i++)
+    for (int j = 0; j < 9; j++)
+      if (board[i][j] == -1)
+        found = 1;
+  if(!found){
+    cout << "You win!! :D)" << endl;
+    storemk();
+    endgame = 1;
+  }
+}
+
+
 
 int main(){
   genboard();
   while (endgame == 0){
-    pntboard();
-    mvboard();
+  pntboard();
+  mvboard();
+  endchk();
   }
   return 0;
 }
